bbca5585a67143b219ada6f713f3cc931f3db90d
cunitac <cunitac@gmail.com>
2021-12-22 11:44:59 +0900

    さらに proconio に寄せた

--- README.md
+++ README.md
@@ -2,23 +2,18 @@
 
 [proconio](https://docs.rs/proconio) が使えない競技プログラミング等で使う、Rust で標準入力をパースするライブラリである。
 
-[src/lib.rs](src/lib.rs) 全体をそのまま貼り付け、必要であれば `use minimal_input::marker::Usize1;` などを書けば、proconio と同様に使うことができる。
+[src/lib.rs](src/lib.rs) 全体をそのまま貼り付け、必要であれば `use minimal_input::marker::Usize1;` などを書けば、proconio と同様に使うことができる。短いコードでほぼ同等の機能を自走している
 
 相違点を列挙しておくが、他にも細かい違いがあるかもしれない。
 
 - モジュール名が異なる
   - ソースコード中の `minimal_input` を `proconio` に置換すれば同じになる
 - proconio では lazy_static を使っているが、minimal_input では thread_local を使っており、標準ライブラリ以外への依存がない
-- proconio では `from source` として入力を指定することができるが、minimal_input ではできない（できるようにすることも検討している） 
 - proconio では行単位で入力を受け取る（LineSource）か一気に入力を受け取る（OnceSource）か選ぶことができるが、minimal_input では行単位で固定である
   - 一気に入力したい場合は、[23行目](src/lib.rs#L23) の `read_line` を `read_to_string` に書き換える
-- minimal_input の実装はかなり短い（約100行）
-- minimal_input には read マクロが存在する
-  - `read!(u32, [i32])` は `{ input!(x: (u32, [i32])); x }` と等価
-  - `read!(@source, u32)` は proconio の `{ input!(from source, x: u32); x }` に相当
-- Source の実装が異なり、そのため Readable の実装方法が異なる
-  - Source が実装するメソッドは next_token のみであり、これは proconio::source:Source の next_token_unwrap に相当する
-  - 前述のように `from source` が存在しないため、代わりに read マクロを用いる
+- Source の実装が異なる
+  - Source は trait ではなく struct であり、Stdin 以外からの入力はできない
+  - Source は is_empty を実装せず、すなわち is_stdin_empty も実装しない
 - fastout は存在しない
 
 ## ライセンス
--- src/lib.rs
+++ src/lib.rs
@@ -3,91 +3,96 @@ pub mod minimal_input {
     use std::fmt::Debug;
     use std::io::{stdin, BufRead, BufReader, Stdin};
     use std::str::{FromStr, SplitWhitespace};
-
     thread_local!(
-        pub static STDIN: RefCell<Source> = RefCell::new(Source {
+        #[doc(hidden)]
+        pub static STDIN_SOURCE: RefCell<Source> = RefCell::new(Source {
             stdin: BufReader::new(stdin()),
             tokens: "".split_whitespace(),
         });
     );
-
     pub struct Source {
         stdin: BufReader<Stdin>,
         tokens: SplitWhitespace<'static>,
     }
-
     impl Source {
-        pub fn next_token(&mut self) -> &str {
-            self.tokens.next().unwrap_or_else(|| {
+        pub fn next_token(&mut self) -> Option<&str> {
+            self.tokens.next().or_else(|| {
                 let mut input = String::new();
                 self.stdin.read_line(&mut input).unwrap();
                 self.tokens = Box::leak(input.into_boxed_str()).split_whitespace();
-                self.tokens.next().unwrap()
+                self.tokens.next()
             })
         }
+        pub fn next_token_unwrap(&mut self) -> &str {
+            self.next_token().unwrap()
+        }
     }
-
+    #[doc(hidden)]
     #[macro_export]
-    macro_rules! read {
-        (@$s:expr, [$t:tt; $n:expr]) => {
-            (0..$n).map(|_| $crate::read!(@$s, $t)).collect::<::std::vec::Vec<_>>()
+    macro_rules! read_value {
+        (from $s:expr, [$t:tt; $n:expr]) => {
+            (0..$n).map(|_| $crate::read_value!(from $s, $t)).collect::<::std::vec::Vec<_>>()
         };
-        (@$s:expr, [$t:tt]) => {{
-            let n = $crate::read!(@$s, usize);
-            $crate::read!(@$s, [$t; n])
+        (from $s:expr, [$t:tt]) => {{
+            let n = $crate::read_value!(from $s, usize);
+            $crate::read_value!(from $s, [$t; n])
         }};
-        (@$s:expr, ($($t:tt),* $(,)?)) => {
-            $crate::read!(@$s, $($t),*)
+        (from $s:expr, ($($t:tt),* $(,)?)) => {
+            $crate::read_value!(from $s, $($t),*)
         };
-        (@$s:expr, $t:ty) => {
+        (from $s:expr, $t:ty) => {
             <$t as $crate::minimal_input::Readable>::read(&mut $s)
         };
-        (@$s:expr, $($t:tt),* $(,)?) => {
-            ($($crate::read!(@$s, $t)),*)
+        (from $s:expr, $($t:tt),* $(,)?) => {
+            ($($crate::read_value!(from $s, $t)),*)
         };
         ($($r:tt)*) => {
             $crate::minimal_input::STDIN.with(|s| {
                 let mut s = s.borrow_mut();
-                $crate::read!(@s, $($r)*)
+                $crate::read_value!(from s, $($r)*)
             })
         }
     }
-
     #[macro_export]
     macro_rules! input {
         () => {};
         ($x:tt: $t:tt, $($r:tt)*) => {
-            let $x = read!($t);
+            let $x = read_value!($t);
             $crate::input!($($r)*);
         };
         (mut $x:tt: $t:tt, $($r:tt)*) => {
-            let mut $x = $crate::read!($t);
+            let mut $x = $crate::read_value!($t);
             $crate::input!($($r)*);
+        };
+        (from $s:expr, $x:tt: $t:tt, $($r:tt)*) => {
+            let $x = read_value!(from $s, $t);
+            $crate::input!(from $s, $($r)*);
+        };
+        (from $s:expr, mut $x:tt: $t:tt, $($r:tt)*) => {
+            let mut $x = $crate::read_value!(from $s, $t);
+            $crate::input!(from $s, $($r)*);
         };
         ($($r:tt)*) => {
             $crate::input!($($r)*,);
         };
     }
-
     pub trait Readable {
         type Output;
         fn read(source: &mut Source) -> Self::Output;
     }
-
     impl<T: FromStr<Err = E>, E: Debug> Readable for T {
         type Output = T;
         fn read(source: &mut Source) -> T {
-            source.next_token().parse().unwrap()
+            source.next_token_unwrap().parse().unwrap()
         }
     }
-
     pub mod marker {
         macro_rules! impl_readable {
             ($e:ty, $t:ty, $u:ty, $f:expr) => {
                 impl $crate::minimal_input::Readable for $e {
                     type Output = $t;
                     fn read(mut source: &mut $crate::minimal_input::Source) -> $t {
-                        $f($crate::read!(@source, $u))
+                        $f($crate::read_value!(from source, $u))
                     }
                 }
             }
